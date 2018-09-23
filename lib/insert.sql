d437371394d0c0752c6fba4bc07ecfa5f6e59ca8
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-09-23 20:41:02 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,78 @@
+INSERT INTO bears
+VALUES (1,
+        "Mr. Chocolate",
+        17,
+        "Male",
+        "Blue",
+        "Friendly",
+        0);
+
+
+INSERT INTO bears
+VALUES (2,
+        "Rowdy",
+        19,
+        "Male",
+        "Blue",
+        "Friendly",
+        1);
+
+
+INSERT INTO bears
+VALUES (3,
+        "Tabitha",
+        27,
+        "Female",
+        "Blue",
+        "Friendly",
+        0);
+
+
+INSERT INTO bears
+VALUES (4,
+        "Sergeant Brown",
+        23,
+        "Male",
+        "Blue",
+        "Friendly",
+        1);
+
+
+INSERT INTO bears
+VALUES (5,
+        "Melissa",
+        15,
+        "Female",
+        "Blue",
+        "Friendly",
+        1);
+
+
+INSERT INTO bears
+VALUES (6,
+        "Grinch",
+        19,
+        "Male",
+        "Green",
+        "Friendly",
+        1);
+
+
+INSERT INTO bears
+VALUES (7,
+        "Wendy",
+        12,
+        "Female",
+        "Brown",
+        "Friendly",
+        0);
+
+
+INSERT INTO bears
+VALUES (8,
+        NULL,
+        14,
+        "Male",
+        "Blue",
+        "Friendly",
+        1);
