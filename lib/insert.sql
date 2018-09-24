3b40e725b76b49cc9be47a4aa29dca2ae26da89f
Ed Putans <edputans@gmail.com>
2018-09-24 09:35:54 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+insert into bears (name,age,gender,color,temperament,alive) values ('Mr. Chocolate',3,'M','Brown','cute',1);
+insert into bears (name,age,gender,color,temperament,alive) values ('Rowdy',12,'M','Brown','grumpy',1);
+insert into bears (name,age,gender,color,temperament,alive) values ('Tabitha',10,'F','Black','cool',1);
+insert into bears (name,age,gender,color,temperament,alive) values ('Sergeant Brown',15,'M','Brown','cowardly',1);
+insert into bears (name,age,gender,color,temperament,alive) values ('Melissa',24,'F','Brown','goofy',0);
+insert into bears (name,age,gender,color,temperament,alive) values ('G
+rinch',10,'M','Brown','stressed',1);
+insert into bears (name,age,gender,color,temperament,alive) values ('Wendy',10,'F','Brown','aggressive',1);
+insert into bears (age,gender,color,temperament,alive) values (8,'F','Brown','killer',1);
